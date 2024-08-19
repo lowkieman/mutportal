f9f6a49bbf2b33cd68aa486e39ae3e49be2b3bd5
lowkieman <lowkieman@gmail.com>
2024-08-19 22:23:02 +0300

    added footer js for the date

---
+++ js/home.js
@@ -0,0 +1,4 @@
+const getYear= new Date().getFullYear();
+document.getElementById("getYear").innerHTML=getYear;
+document.getElementById("getYear1").innerHTML=getYear;
+document.getElementById("getYear2").innerHTML=getYear;
